264ab29f3bcc32330743267e7092f46abc32ff5b
Blake Montgomery <blakemontgomery312@gmail.com>
2021-07-06 11:16:04 -0500

    Update the Model

--- models/dish.js
+++ models/dish.js
@@ -2,19 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const dishSchema = new Schema({
+    // the basic information
     name: { type: String, required: true },
     price: { type: String, required: true },
     description: { type: String, required: true },
-    starter: { type: Boolean, required: true },
-    main: { type: Boolean, required: true },
-    dessert: { type: Boolean, required: true },
+
+    // which menu?
+    drink: { type: Boolean, required: false },
+    dinner: { type: Boolean, default: true },
+    dessert: { type: Boolean, required: false },
+
+    // dinner menu subdivision
+    starter: { type: Boolean, required: false },
+    main: { type: Boolean, required: false },
+    largeFormat: { type: Boolean, default: false },
+
+    // dietary restrictions
     vegetarian: { type: Boolean, default: false },
     vegan: { type: Boolean, default: false },
     glutenFree: { type: Boolean, default: false },
-    largeFormat: { type: Boolean, default: false },
+    
 },  {
         timestamps: true
-    });
+    }
+);
 
 const Dish = mongoose.model("Dish", dishSchema );
 
