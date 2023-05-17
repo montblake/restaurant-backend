48fe9810a7a0f18e94c7509e28f9e83305014042
Blake Montgomery <blakemontgomery312@gmail.com>
2023-05-17 17:01:28 -0500

    Update Validation

--- validations/menuItemValidation.js
+++ validations/menuItemValidation.js
@@ -32,10 +32,12 @@ const menuItemValidationRules = () => {
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
+    console.log("No errors");
     return next()
   }
   const extractedErrors = [];
   errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+  console.log("Errors: ", extractedErrors);
   return res.status(422).json({ errors: extractedErrors });
 }
 
