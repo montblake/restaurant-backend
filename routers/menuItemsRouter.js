c68178b2c949f5b5da2437c570f614388fde1210
Blake Montgomery <blakemontgomery312@gmail.com>
2023-05-22 16:54:37 -0500

    Remove Old Route

--- routers/menuItemsRouter.js
+++ routers/menuItemsRouter.js
@@ -4,22 +4,6 @@ const router = express.Router();
 const MenuItem = require("../models/menuItem");
 const { menuItemValidationRules, validate } = require("../validations/menuItemValidation");
 
-// by gatherign courses by menu programmaticaly, we can ensure info is up-to-date
-// downside: we lost control over specific order of courses on menu
-// TODO: add a field to MenuItem model to allow for manual ordering of courses
-// router.get("/courses-by-menu", async (req, res, next) => {
-//   try {
-//     const menuTypes = await MenuItem.distinct("menuType");
-//     const coursesByMenuType = {};
-//     for (const menuType of menuTypes) {
-//       coursesByMenuType[menuType] = await MenuItem.find({ menuType: menuType }).distinct("course");
-//     }
-//     res.json(coursesByMenuType);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 router.get("/", async (req, res, next) => {
   try {
     const courseId = req.query.course;
